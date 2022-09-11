49127c8901458b838faa5101bfa16575f414bb4e
Kiwiphoenix <98428865+kiwiphoenix364@users.noreply.github.com>
2022-09-10 22:56:51 -0400

    Initial files for MakeCode project

---
+++ ImageGenerator.ts
@@ -0,0 +1,19 @@
+namespace redrawImg {
+    //% block
+    export function GenerateImage() {
+        for (let x = 0; x < 255; x ++) {
+            for (let y = 0; y < 255; y++) {
+                drawImg(x * 16, y * 16, tiles.tileImageAtLocation(tiles.getTileLocation(x, y)), trackImage)
+            }
+        }
+    }
+    function drawImg(x: number, y: number, startImg: Image, targetImg: Image) {
+        for (let xIdx = 0; xIdx < 16; xIdx++) {
+            for (let yIdx = 0; yIdx < 16; yIdx++) {
+                if (startImg.getPixel(xIdx, yIdx) != 0) {
+                    targetImg.setPixel(x + xIdx, y + yIdx, startImg.getPixel(xIdx, yIdx))
+                }
+            }
+        }
+    }
+}
